073056ed0d5ff24d0a381bbb3d2172e713835106
Jemima mbingu <beautinahmbingu@gmail.com>
2026-07-19 11:36:43 +0200

    Add root redirect to results homepage

--- school_system/urls.py
+++ school_system/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('results.urls')),
+    path('', RedirectView.as_view(url='/results/', permanent=False)),
+    path('results/', include('results.urls')),
 ]
